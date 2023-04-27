c9f8eca7cdfa3b8d250b655491608aaf9e6a3a21
2005balaji <balajisr000@gmail.com>
2023-04-27 09:50:40 +0530

    added all my classmates API links

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const https = require('https');
 
 const urls = [
   'https://slippy.onrender.com/',
-  'https://google.com/',
+  'https://dsa101.onrender.com/',
+  'https://invitemate.onrender.com/',
+  'https://digislam.onrender.com/digislam/apis/users',
+  'https://blynd-server.onrender.com/',
+  'https://phonewise.onrender.com/api/brands',
+  'https://thedailydigest.onrender.com/',
+  'https://queueup.onrender.com/',
+  'https://brickel-server.onrender.com/',
+  'https://bookstop.onrender.com/api/product',
+  'https://cravesavor2.onrender.com/',
+  'https://agroshield.up.railway.app/userinput?cropName=rice'
   // Add more URLs here
 ];
 
